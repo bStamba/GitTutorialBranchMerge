6287740f0a614e4e746858d21081ccde3b4c61c2
Brandon Stamba <brandonstamba@gmail.com>
2023-07-13 15:11:08 -0500

    Version 1 Branching and Merging

---
+++ src/GitTutorial4Package/GitTutorial4.java
@@ -0,0 +1,8 @@
+package GitTutorial4Package;
+
+public class GitTutorial4 {
+
+	public void getMessage() {
+		System.out.println("This is an exercise in Branching and Merging.");
+	}
+}
---
+++ src/GitTutorial4Package/GitTutorial4Main.java
@@ -0,0 +1,11 @@
+package GitTutorial4Package;
+
+public class GitTutorial4Main {
+
+	public static void main(String[] args) {
+		
+		GitTutorial4 g = new GitTutorial4();
+		g.getMessage();
+		System.out.println("This is Version 1 of Branching and Merging");
+	}
+}
